4ce1987862d9c465dcbcfb4a4b2634b67bc7ed65
Prashant Srivastava <82441036+prashantmagnifiedcode@users.noreply.github.com>
2021-09-08 08:23:27 +0530

    newb

--- src/reducer/combine.jsx
+++
@@ -1,8 +0,0 @@
-import Changenumbersecond from '../reducer/multi'
-import Changenumber from '../reducer/updown'
-import TodoReducer from './todo'
-
-
-import {combineReducers} from "redux"
-const rootReducer = combineReducers({Changenumber,Changenumbersecond,TodoReducer})
-export default rootReducer;
